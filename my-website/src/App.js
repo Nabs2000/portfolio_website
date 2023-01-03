16cea3fe99aff93b2fe8d2fb12558be6bbc3cd87
Nabeel Sabzwari <nabz101@berkeley.edu>
2023-01-03 15:56:03 -0800

    Move React components inside folder and update index.css

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,7 +1,25 @@
+import React from "react";
+import { Header } from "./components/header/Header";
+import { Nav } from "./components/nav/Nav";
+import { About } from "./components/about/About";
+import { Contact } from "./components/contact/Contact";
+import { Experience } from "./components/experience/Experience";
+import { Footer } from "./components/footer/Footer";
+import { Portfolio } from "./components/portfolio/Portfolio";
+import { Services } from "./components/services/Services";
+import { Testimonials } from "./components/testimonials/Testimonials";
 export function App() {
   return (
-    <div>
-      <p>Test App</p>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   );
 }
