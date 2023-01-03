a1d3d4dd263ccf22bda231f71e32a37a49fdf53a
Nabeel Sabzwari <nabz101@berkeley.edu>
2023-01-03 12:25:07 -0800

    Begin website creation with different design

--- my-website/src/index.js
+++ my-website/src/index.js
@@ -1,17 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { AboutMe, App, Contact, Experience } from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/about" element={<AboutMe />} />
-      <Route path="/experience" element={<Experience />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+import { App } from "./App";
+ReactDOM.render(<App />, document.querySelector("#root"));
