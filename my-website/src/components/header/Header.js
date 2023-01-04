1dbbb04318fd714050c9aef100c2c42f6709bae6
Nabeel Sabzwari <nabz101@berkeley.edu>
2023-01-04 11:48:45 -0800

    Update Header component

--- my-website/src/components/header/Header.js
+++ my-website/src/components/header/Header.js
@@ -1,3 +1,24 @@
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import PFP from "../../assets/pfp-remove-bg.png";
+import HeaderSocials from "./HeaderSocials";
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I'm</h5>
+        <h1>Nabeel Sabzwari</h1>
+        <h5 className="text-light">Bioengineering Grad</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={PFP} alt="Me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 }
