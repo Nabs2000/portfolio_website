214749881299874aac2a844f661f5424171fff21
Nabeel Sabzwari <nabz101@berkeley.edu>
2023-01-04 11:49:36 -0800

    Add Resume component for website header

---
+++ my-website/src/components/header/CTA.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Resume from "../../assets/Nabeel_Sabzwari_Resume.pdf";
+const CTA = () => {
+  return (
+    <div className="cta">
+      <a href={Resume} download className="btn">
+        Download Resume
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's Connect!
+      </a>
+    </div>
+  );
+};
+
+export default CTA;
