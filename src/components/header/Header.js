fe8cf416406711f4892004c667f45862a7dd52ca
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2023-09-16 11:11:46 -0700

    Update header short description

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import PFP from "../../assets/pfp-remove-bg.png";
+import PFP from "../../assets/pfp.jpeg";
 import HeaderSocials from "./HeaderSocials";
 export function Header() {
   return (
@@ -9,7 +9,7 @@ export function Header() {
       <div className="container header__container">
         <h5>Hello, I'm</h5>
         <h1>Nabeel Sabzwari</h1>
-        <h5 className="text-light">Software Enthusiast</h5>
+        <h5 className="text-light">M.S. in Computer Science, UC Davis</h5>
         <CTA />
         <HeaderSocials />
         <div className="me">
