4fd51faa783450eecd59c90d596a45a398ef25d3
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2023-09-16 08:34:39 -0700

    Update skills and experiences icon links and pics

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,18 +1,49 @@
-import React from 'react'
-import "./nav.css"
-import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
-import {RiSuitcase2Line, RiServiceLine} from 'react-icons/ri'
-import {BiPhoneCall} from 'react-icons/bi'
-import { useState } from 'react'
+import React from "react";
+import "./nav.css";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { MdWorkOutline } from "react-icons/md";
+import { BiPhoneCall } from "react-icons/bi";
+import { useState } from "react";
+import { BsCardChecklist } from "react-icons/bs";
 export function Nav() {
-  const [activeNav, setActiveNav] = useState("#")
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href='#' className={activeNav === '#' ? 'active' : ''} onClick={() => setActiveNav('#')}><AiOutlineHome /></a>
-      <a href='#about' className={activeNav === '#about' ? 'active' : ''} onClick={() => setActiveNav('#about')}><AiOutlineUser /></a>
-      <a href='#experience' className={activeNav === '#experience' ? 'active' : ''} onClick={() => setActiveNav('#experience')}><RiSuitcase2Line /></a>
-      <a href='#services' className={activeNav === '#services' ? 'active' : ''} onClick={() => setActiveNav('#services')}><RiServiceLine /></a>
-      <a href='#contact' className={activeNav === '#contact' ? 'active' : ''} onClick={() => setActiveNav('#contact')}><BiPhoneCall /></a>
+      <a
+        href="#"
+        className={activeNav === "#" ? "active" : ""}
+        onClick={() => setActiveNav("#")}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        className={activeNav === "#about" ? "active" : ""}
+        onClick={() => setActiveNav("#about")}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#skills"
+        className={activeNav === "#skills" ? "active" : ""}
+        onClick={() => setActiveNav("#skills")}
+      >
+        <BsCardChecklist />
+      </a>
+      <a
+        href="#experiences"
+        className={activeNav === "#experiences" ? "active" : ""}
+        onClick={() => setActiveNav("#experiences")}
+      >
+        <MdWorkOutline />
+      </a>
+      <a
+        href="#contact"
+        className={activeNav === "#contact" ? "active" : ""}
+        onClick={() => setActiveNav("#contact")}
+      >
+        <BiPhoneCall />
+      </a>
     </nav>
-  )
+  );
 }
