a402e36e2ccab523060725c64622ae5b47c782a4
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2023-09-16 09:15:20 -0700

    Update icon links in header section

--- src/components/header/HeaderSocials.js
+++ src/components/header/HeaderSocials.js
@@ -4,13 +4,16 @@ import { BsLinkedin, BsGithub, BsFacebook } from "react-icons/bs";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank">
+      <a
+        href="https://linkedin.com/in/nabeel-sabzwari-899697167/"
+        target="_blank"
+      >
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank">
+      <a href="https://github.com/Nabs2000" target="_blank">
         <BsGithub />
       </a>
-      <a href="https://facebook.com" target="_blank">
+      <a href="https://facebook.com/nabeel.sabzwari" target="_blank">
         <BsFacebook />
       </a>
     </div>
