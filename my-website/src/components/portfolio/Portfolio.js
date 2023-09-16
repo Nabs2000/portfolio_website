601161f9bfbe5dd0c7c796062d3886a0a4d78868
Nabeel Sabzwari <nabz101@berkeley.edu>
2023-09-15 23:17:56 -0700

    Update portfolio info and styling

--- my-website/src/components/portfolio/Portfolio.js
+++ my-website/src/components/portfolio/Portfolio.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./portfolio.css";
-import MCPic from "../../assets/monte_carlo_portfolio_img.png";
+import ReactPic from "../../assets/react_logo_portfolio_img.png";
 import NGordnetPic from "../../assets/ngordnet_portfolio_img.png";
 import DronePic from "../../assets/drone_delivery_portfolio_img.png";
 import RTGPic from "../../assets/result_gen_tool_portfolio_img.png";
@@ -10,45 +10,39 @@ import DequePic from "../../assets/deque_portfolio_img.png";
 const data = [
   {
     id: 1,
-    image: MCPic,
-    title: "Monte Carlo Simulation",
-    github: "https://github.com",
-    demo: "https://google.com",
+    image: ReactPic,
+    title: "This Website!",
+    github: "https://github.com/Nabs2000/my_website",
   },
   {
     id: 2,
     image: NGordnetPic,
-    title: "NGordnet: A Variation of Wordnet",
-    github: "https://github.com",
-    demo: "https://google.com",
+    title: "NGordnet: Graph Project",
+    github: "https://github.com/Nabs2000/cs61b_2022/tree/main/proj2",
   },
   {
     id: 3,
     image: DronePic,
-    title: "Autonomous Drone Delivery System",
-    github: "https://github.com",
-    demo: "https://google.com",
+    title: "Drone Delivery System",
+    github: "https://github.com/Nabs2000/eecs106a_final_proj/tree/main",
   },
   {
     id: 4,
     image: RTGPic,
     title: "Result Generator Tool",
-    github: "https://github.com",
-    demo: "https://google.com",
+    github: "https://github.com/Nabs2000/result_generator_tool",
   },
   {
     id: 5,
     image: GamePic,
     title: "2048 Project",
-    github: "https://github.com",
-    demo: "https://google.com",
+    github: "https://github.com/Nabs2000/cs61b_2022/tree/main/proj0",
   },
   {
     id: 6,
     image: DequePic,
     title: "Deque Project",
-    github: "https://github.com",
-    demo: "https://google.com",
+    github: "https://github.com/Nabs2000/cs61b_2022/tree/main/proj1",
   },
 ];
 export function Portfolio() {
@@ -58,7 +52,7 @@ export function Portfolio() {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {data.map(({ id, image, title, github, demo }) => {
+        {data.map(({ id, image, title, github }) => {
           return (
             <article key={id} className="portfolio__item">
               <div className="portfolio__item-image">
@@ -67,9 +61,6 @@ export function Portfolio() {
                 <div className="portfolio__item-cta">
                   <a href={github} className="btn" target="_blank">
                     Github
-                  </a>
-                  <a href={demo} className="btn btn-primary" target="_blank">
-                    Live Demo
                   </a>
                 </div>
               </div>
