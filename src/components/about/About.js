7f3419fbdb1a178e10a56b58761a77f2b46f0a76
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2023-09-16 11:48:15 -0700

    Delete cards and pic from about section, and delete CSS

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./about.css";
-import ME from "../../assets/pfp.jpeg";
 import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { VscFolderLibrary } from "react-icons/vsc";
@@ -11,32 +10,12 @@ export function About() {
       <h2>About Me</h2>
 
       <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-image">
-            <img src={ME} alt="About Image" />
-          </div>
-        </div>
         <div className="about__content">
-          <div className="about__cards">
-            <article className="about__card">
-              <FaAward className="about__icon" />
-              <h5>Experience</h5>
-              <small>2+ Years Working</small>
-            </article>
-            <article className="about__card">
-              <FiUsers className="about__icon" />
-              <h5>Clients</h5>
-              <small>50+ Clients Nationwide</small>
-            </article>
-            <article className="about__card">
-              <VscFolderLibrary className="about__icon" />
-              <h5>Projects</h5>
-              <small>5+ Projects Completed</small>
-            </article>
-          </div>
           <p>
             Hi there! My name is Nabeel Sabzwari, and I am a master's student at
-            UC Davis majoring in computer science.
+            UC Davis majoring in computer science. I am currently seeking a
+            software engineering internship to contribute to company objectives
+            while nurturing my own passion for problem-solving and innovation.
           </p>
 
           <a href="#contact" className="btn btn-primary">
