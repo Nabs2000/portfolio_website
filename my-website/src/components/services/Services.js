f9a2c5dcd36b7cff0e674700f6f23521b1be6189
Nabeel Sabzwari <nabz101@berkeley.edu>
2023-09-15 23:19:25 -0700

    Update service info to include work experiences

--- my-website/src/components/services/Services.js
+++ my-website/src/components/services/Services.js
@@ -4,102 +4,89 @@ import { BiCheck } from "react-icons/bi";
 export function Services() {
   return (
     <section id="services">
-      <h5>What I Offer</h5>
-      <h2>Services</h2>
+      <h5>What I Did</h5>
+      <h2>Work Experience</h2>
       <div className="container services__container">
         <article className="service">
           <div className="service__head">
-            <h3>UI/UX Design</h3>
+            <h3>iRhythm Technologies</h3>
           </div>
 
           <ul className="service__list">
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
+              <p>
+                Played a pivotal role in the design verification and validation
+                of iRhythm’s next-gen Zio AT device.
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
+              <p>
+                Proactively identified and reported firmware bugs in Jira during
+                testing, contributing to product quality improvements.
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
+              <p>
+                Automated two tests during the test cycle, resulting in a
+                remarkable 83% and 99% time reduction, respectively.
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
+              <p>
+                Leveraged expertise in data structures and algorithms in Python
+                to automate daily tasks for both personal and team-wide use.
+              </p>
             </li>
           </ul>
         </article>
         <article className="service">
           <div className="service__head">
-            <h3>Web Development</h3>
+            <h3>Xtrava Health</h3>
           </div>
 
           <ul className="service__list">
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
+              <p>
+                Created a parsing script using Python to extract valuable
+                insights for 500 scanned tests evaluating the performance of
+                Xtrava Health’s COVID-19 digital sensors.
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
+              <p>
+                Implemented scripts in Python to visualize and pinpoint areas of
+                improvement in Xtrava's COVID-19 detection algorithm.
+              </p>
             </li>
           </ul>
         </article>
         <article className="service">
           <div className="service__head">
-            <h3>Content Creation</h3>
+            <h3>Nanotechnology Lab, UC Berkeley</h3>
           </div>
 
           <ul className="service__list">
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
+              <p>
+                Wrote a Python program for interactivity between a Raspberry Pi
+                and Arduino for a dialysis device in the lab of Dr. Waqas Khalid
+                through Berkeley’s URAP program.
+              </p>
             </li>
             <li>
               <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
-            </li>
-            <li>
-              <BiCheck className="service__list-icon" />
-              <p>Lorem, impsum</p>
+              <p>
+                Designed front-end Python code hosted on a Raspberry Pi using Qt
+                Creator to be utilized by doctors for a graphical user
+                interface.
+              </p>
             </li>
           </ul>
         </article>
